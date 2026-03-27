ff9c5a4823584241609eb872eaf70c468de48d3c
rat371 <104175515+rat371@users.noreply.github.com>
2026-03-27 11:01:57 +1100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,8 +32,12 @@ if ingredients_list:
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
         
-        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+        filtered = pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON']
+    if not filtered.empty:
+        search_on = filtered.iloc[0]
+        st.write(f"The search value for {fruit_chosen} is {search_on}.")
+    else:
+        st.write(f"No search value found for {fruit_chosen}.")
         
         st.subheader(fruit_chosen + ' Nutrition Information')
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen) 
